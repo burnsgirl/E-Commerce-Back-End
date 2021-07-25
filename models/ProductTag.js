3f7381ccc98dc8717a5bf6fe1eb865568a84cd22
Dauphine Burns <dauphineburns@gmail.com>
2021-07-25 15:56:20 -0400

    product tag model updated

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,25 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: 'id'
+      }
+    },
+    tag_id: {
+      references: {
+        model: Tag,
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,

e0c2e819af3a39cb5e98339d95ca450892ec93b0
Dauphine Burns <dauphineburns@gmail.com>
2021-07-25 18:14:47 -0400

    syntax error on model index belongs

--- models/index.js
+++ models/index.js
@@ -13,12 +13,12 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id'
 });

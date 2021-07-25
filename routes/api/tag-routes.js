4ff302e0ab161f4af7ed79d4576c86c7b7988489
Dauphine Burns <dauphineburns@gmail.com>
2021-07-25 17:46:53 -0400

    tag route functions created

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,56 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{model: Product}]}).then((tagData) => {
+    res.json(tagData);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    }, include: [{model: Product}]}).then((res) => {
+    res.json(res);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((tag) => {
+    res.json(tag);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      id: req.body.id
+    },
+    {
+      where: {
+        id: req.params.id
+      },
+    },
+  ).then ((updatedTag) => {
+    res.json(updatedTag);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    },
+  }).then((deletedTag) => {
+    res.json(deletedTag);
+  });
 });
 
 module.exports = router;

ae47348ef109c250241db2fecc2f64e1656d3fd4
Dauphine Burns <dauphineburns@gmail.com>
2021-07-25 15:52:40 -0400

    product model updated

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      isNumeric: true, 
+      defaultValue: 10,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,

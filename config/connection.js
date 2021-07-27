b376221df5ebf5fb4377624c47deeab69595a0d8
Dauphine Burns <dauphineburns@gmail.com>
2021-07-27 15:28:54 -0400

    changed C to lower c in product js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+
+
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
@@ -7,9 +9,11 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
     });
 
 module.exports = sequelize;
+
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
+//Added to try
+const seed = require('./seed');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
